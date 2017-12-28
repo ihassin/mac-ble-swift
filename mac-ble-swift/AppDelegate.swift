209eeb9c2dc0eaff4ed6a129bb7b327911ddb11b
Itamar Hassin <ihassin@mac.com>
2017-12-27 19:44:45 -0500

    Initial Commit

---
+++ mac-ble-swift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  mac-ble-swift
+//
+//  Created by Itamar Hassin on 12/27/17.
+//  Copyright © 2017 Itamar Hassin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ mac-ble-swift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  mac-ble-swift
+//
+//  Created by Itamar Hassin on 12/27/17.
+//  Copyright © 2017 Itamar Hassin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
